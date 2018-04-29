c9c8c0565105788284955cecdabcfc3364c2c2f6
Mark Proctor <mproctor@codehaus.org>
2018-04-29 01:25:30 +0100

    -Fixed docking offsets, when the path BB does not have a minX and minY of 0. -The snaps adjustDistance was the wrong way around, which gave weird behaviour while dragging a docked item. -For issue where PolyLine adjusts enhancement breaks OrthogonalLine, which does not impl the method and returns null. -execute() is expected to set the location. -setLocation now calls shapeMoved()

--- src/main/java/com/ait/lienzo/client/core/shape/wires/handlers/impl/WiresShapeControlImpl.java
+++ src/main/java/com/ait/lienzo/client/core/shape/wires/handlers/impl/WiresShapeControlImpl.java
@@ -159,7 +159,7 @@ public class WiresShapeControlImpl
         if (isDockAdjust) {
             final Point2D dadjust = m_dockingAndControl.getAdjust();
             double adjustDistance = Geometry.distance(dx, dy, dadjust.getX(), dadjust.getY());
-            if (adjustDistance > getWiresManager().getDockingAcceptor().getHotspotSize()) {
+            if (adjustDistance < getWiresManager().getDockingAcceptor().getHotspotSize()) {
                 dxy.setX(dadjust.getX());
                 dxy.setY(dadjust.getY());
             }
@@ -246,15 +246,13 @@ public class WiresShapeControlImpl
         if (!accept) {
             throw new IllegalStateException("Execute should not be called. No containment neither docking operations have been accepted.");
         }
-        final Point2D location = c_accept ?
-                getContainmentControl().getCandidateLocation() :
-                getDockingControl().getCandidateLocation();
+
+        // It's expected the execute() also sets the location
         if (d_accept) {
             getDockingControl().execute();
         } else {
             getContainmentControl().execute();
         }
-        getParentPickerControl().setShapeLocation(location);
         ShapeControlUtils.checkForAndApplyLineSplice(getWiresManager(),
                                                      getShape());
         shapeUpdated(true);
@@ -346,7 +344,6 @@ public class WiresShapeControlImpl
     private void shapeUpdated(final boolean isAcceptOp) {
         ShapeControlUtils.updateSpecialConnections(m_connectorsWithSpecialConnections,
                                                    isAcceptOp);
-        ShapeControlUtils.updateNestedShapes(getShape());
     }
 
     private void clearState() {
